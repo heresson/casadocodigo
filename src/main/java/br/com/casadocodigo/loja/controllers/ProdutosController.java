0883a53f51d94b00af36ca57ac87783c04d73fc2
heresson <heresson@mppa.mp.br>
2017-10-13 13:06:57 -0300

    envio de codigo ate aula 3

---
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -0,0 +1,34 @@
+package br.com.casadocodigo.loja.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.casadocodigo.loja.daos.ProdutoDao;
+import br.com.casadocodigo.loja.models.Produto;
+import br.com.casadocodigo.loja.models.TipoPreco;
+
+@Controller
+public class ProdutosController {
+	
+	@Autowired
+    private ProdutoDao dao;
+
+	@RequestMapping("/produtos/form")
+	public ModelAndView form() {
+		
+		ModelAndView modelAndView = new ModelAndView("produtos/form");
+        modelAndView.addObject("tipos", TipoPreco.values());
+
+        return modelAndView;
+	}
+	
+	@RequestMapping("/produtos")
+    public String grava(Produto produto) {
+        System.out.println(produto);
+        dao.gravar(produto);
+        return "produtos/ok";
+    }
+	
+}
